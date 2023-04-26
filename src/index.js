f47316f2603ab32f6a906dabb0027e3717a76a84
fernandacaniçais <fernanda.canicais@outlook.com>
2023-04-26 17:55:56 -0300

    atualização 26/04

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import validator from './validator.js';
 
+// criar cometário
+/*criar comentario várias linhas
+*/
+//pegar o Elemento com o Id=validar que é meu botão e adicionar um evento ao clicar, para validar numero do cartão.
+document.getElementById("validar").addEventListener("click", validarnumerocartao);
+
+//o que acontecerá a partir do clique estará dentro da função abaixo:
+function validarnumerocartao() {
+    const numerocartao = document.getElementById("numero-do-cartao").value;
+    if (validator.isValid(numerocartao)) {
+        const ultimosdig = numerocartao.substring(numerocartao.lenght - 4);
+        const masknumero = validator.maskify(numerocartao);
+        alert ("OK, cartão válido");
+
+    } else {
+        alert ("Oh! cartão inválido");
+    }
+
+}
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,48 @@
 const validator = {
-  // ...
+isValid: function (cartaocredito) {
+cartaocredito = cartaocredito.raplece(/\s/g, "");
+if (!/^\d+$/.validar(cartaocredito)) {
+  return false;
+}
+
+const reversed = cartaocredito.split("").reverse().join("");
+
+//Algoritimo Luhn
+//Para somar o primeiro, pular o seguinte e assim sussecivamente;
+let sum = 0;
+for (let i = 0; i <reversed.lenght; i++){
+  let digit = parseInt(reversed[i]);
+
+//Para Luhn, é necessario inverter o array, o primeiro nº se torna o ultimo;
+if(i % 2!== 0){
+  digit *= 2;
+  if (digit >9){
+    digit -=9;
+  }
+}
+sum += digit;
+}
+return sum %10 === 0;
+}
 };
 
+
+maskify function (cartaocredito) {
+ 
+  if (cartaocredito.length <= 4) {
+    return cartaocredito;
+  }
+
+  const maskLength = cartaocredito.length - 4;
+  let mask = "";
+  for (let i = 0; i < maskLength; i++) {
+    mask += Math.random() < 0.5 ? "#" : "🐱";
+    //A função Math.random() retorna um número pseudo-aleatório no intervalo
+  }
+  return mask + cartaocredito.substring(maskLength);
+  //esse metodo substring extrai caracteres entre indices (posições) de uma string e retorna uma substring
+},
+;
+
 export default validator;
+
